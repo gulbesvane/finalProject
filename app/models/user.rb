dd1b2662a9d1f26a2ccf082deb463822e862d7e4
Ruta <gulbesvane@gmail.com>
2022-06-22 14:30:36 +0100

    Added username and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
-
+    # validate username to be unique, present and its length
+    validates :username, presence: true,
+                 uniqueness: { case_sensitive: false },
+                 length: { minimum: 3, maximum: 25 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    # validate email to be present, unique, length and valid format
+    validates :email, presence: true,
+                 uniqueness: { case_sensitive: false },
+                 length: { maximum: 105 },
+                 format: { with: VALID_EMAIL_REGEX }
 end
