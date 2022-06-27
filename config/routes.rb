ede273b3c5f0c7e595903f681019913e854cfceb
Ruta <gulbesvane@gmail.com>
2022-06-27 12:28:59 +0100

    craete user account form created

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :posts
   #pages controller action home
   root 'pages#home'
+  # signup get request sned to users controlled action new
+  get 'signup', to: 'users#new'
+  # create all routes for users except nes as it is defined already
+  resources :users, except: [:new]
   #showcase get request send to pages controller action showcase
   get 'showcase', to: 'pages#showcase'
   #colab get request send to pages controller colab action
